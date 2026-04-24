d4b83d247605d9e45a8ab126a3963060f828435d
DaveTsai <davetsai@systex.com>
2026-04-24 11:09:18 +0800

    Add Google Analytics integration (GA4) and ignore common system/editor files

---
+++ js/ga.js
@@ -0,0 +1,14 @@
+// Google Analytics (GA4)
+(function() {
+    const GA_MEASUREMENT_ID = 'G-GSNLP35Y1W';
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID);
+})();
